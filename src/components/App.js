a352add5160c16d4b986a4e3370eda020cc1e4bb
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-25 12:43:16 +0000

    partial

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,12 @@ import { Loader } from './Loader';
 import { Todo } from './Todo';
 const App = () => {
   const [dataFetched, setDataFetched] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [isIncompleted, setIsIncompleted] = useState(false);
-
-  const handleCompleted = () => {
-    setIsCompleted(!isCompleted);
-  }
-
-  const handleIncompleted = () => {
-    setIsIncompleted(!isIncompleted);
-  }
+  
 
   return (
     <>
-        {dataFetched ? <Todo /> : <Loader />}
-        Show completed <input type={'checkbox'} value={'Show Complete'} checked={isCompleted} onChange={handleCompleted}/>
-        <br/>
-        Show incompleted <input type={'checkbox'} value={'Show Complete'} checked={isIncompleted} onChange={handleIncompleted}/>
+        {!dataFetched && <Loader />}
+        <Todo dataFetched={dataFetched} setDataFetched={setDataFetched}/>
     </>
   )
   
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,47 @@
- import React from 'react';
+ import React, { useEffect, useState } from 'react';
 
- export const Todo  = () =>{
+ export const Todo  = (props) =>{
+
+    const {dataFetched, setDataFetched} = props;
+    const [isCompleted, setIsCompleted] = useState(true);
+    const [isIncompleted, setIsIncompleted] = useState(true);
+
+    const [fetchedData, setFetchedData] = useState([]);
+
+    useEffect(() => {
+        const fetchTodo = async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+            const json = await response.json();
+            setFetchedData(json);
+            setDataFetched(true);
+        }
+        fetchTodo();
+    },[]);
+
+
+    const handleCompleted = () => {
+        setIsCompleted(!isCompleted);
+    }
+
+    const handleIncompleted = () => {
+        setIsIncompleted(!isIncompleted);
+    }
     
+    return(
+        <>{dataFetched &&
+            <>
+                {
+                    fetchedData.map((obj) => {
+                        
+                    })
+                }
+                <div id="filter-holder">
+                    Show completed <input id="completed-checkbox" type={'checkbox'} value={'Show Complete'} checked={isCompleted} onChange={handleCompleted} />
+                    <br/>
+                    Show incompleted <input id="incompleted-checkbox" type={'checkbox'} value={'Show Complete'} checked={isIncompleted} onChange={handleIncompleted}/>
+                </div>
+            </>
+            }          
+        </>
+    )
  }

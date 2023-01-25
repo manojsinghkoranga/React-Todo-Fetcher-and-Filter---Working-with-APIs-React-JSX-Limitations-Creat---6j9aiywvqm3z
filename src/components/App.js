75a6eabccfce13e481049b5fb4834a955b6db397
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-25 09:20:31 +0000

    partially

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { Todo } from './Todo';
 const App = () => {
-  
+  const [dataFetched, setDataFetched] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [isIncompleted, setIsIncompleted] = useState(false);
+
+  const handleCompleted = () => {
+    setIsCompleted(!isCompleted);
+  }
+
+  const handleIncompleted = () => {
+    setIsIncompleted(!isIncompleted);
+  }
+
+  return (
+    <>
+        {dataFetched ? <Todo /> : <Loader />}
+        Show completed <input type={'checkbox'} value={'Show Complete'} checked={isCompleted} onChange={handleCompleted}/>
+        <br/>
+        Show incompleted <input type={'checkbox'} value={'Show Complete'} checked={isIncompleted} onChange={handleIncompleted}/>
+    </>
+  )
   
 }
 
